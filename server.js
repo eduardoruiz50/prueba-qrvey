61b0904c056366542a2805d8b835715dde93e647
Carlos Ruiz <eduardoruiz50@gmail.com>
2021-10-19 13:20:18 -0500

    heroku deploy changes

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/prueba-qrvey'));
+app.use(express.static('./dist/find-any-country'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/prueba-qrvey/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/find-any-country/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
